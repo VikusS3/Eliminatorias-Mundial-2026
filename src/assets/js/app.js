0c415785fd53f1612c297e655eed842edda3943c
VikusS3 <felix21soc@gmail.com>
2024-04-03 11:38:58 -0500

    add third endpoint

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,7 +12,7 @@ const arrows = {
   ),
 };
 
-const sections = ["id_clasificacion", "id_resultados", "id_proximos_partidos"];
+// const sections = ["id_clasificacion", "id_resultados", "id_proximos_partidos"];
 
 const btns = [
   "btnLastJornada",
@@ -28,88 +28,65 @@ const jornada_numbers = [
   "jornada_number_4",
 ];
 
+const jornada_numbers_proximos = [
+  "jornada_number_1_prox",
+  "jornada_number_2_prox",
+  "jornada_number_3_prox",
+];
+
+const btnsProxJornada = [
+  "btnProximaJornada",
+  "btnPosteriorJornada",
+  "btnUltimaJornada",
+];
+
 function getArrowSvg(color, badge, path) {
   return `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="${color}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-${badge}">
     <path d="M0 0h24v24H0z" stroke="none"/> <path d="${path}"/></svg>`;
 }
 
-const btnPartidos = document
-  .getElementById("btnPartidos")
-  .addEventListener("click", () => {
-    const partidos = (document.getElementById(
-      "id_proximos_partidos"
-    ).style.display = "block");
-    const clasificacion = (document.getElementById(
-      "id_clasificacion"
-    ).style.display = "none");
-    const resultados = (document.getElementById("id_resultados").style.display =
-      "none");
+const buttons = [ "btnClasificacion","btnResultados","btnPartidos"];
+const sections = ["id_clasificacion", "id_resultados", "id_proximos_partidos"];
+const functions = [getClassification,getResults,getPartidos];
 
-    const btnPartidos = document.getElementById("btnPartidos");
-    btnPartidos.classList.add("button-active");
-    const btnResultados = document
-      .getElementById("btnResultados")
-      .classList.remove("button-active");
-    const btnClasificacion = document
-      .getElementById("btnClasificacion")
-      .classList.remove("button-active");
+buttons.forEach((button, index) => {
+  document.getElementById(button).addEventListener("click", () => {
+    sections.forEach((section, i) => {
+      document.getElementById(section).style.display = i === index ? "block" : "none";
+    });
 
-    getPartidos();
+    buttons.forEach((btn, i) => {
+      const element = document.getElementById(btn);
+      if (i === index) {
+        element.classList.add("button-active");
+      } else {
+        element.classList.remove("button-active");
+      }
+    });
+
+    functions[index]();
   });
-
-const btnResultados = document
-  .getElementById("btnResultados")
-  .addEventListener("click", () => {
-    const partidos = (document.getElementById(
-      "id_proximos_partidos"
-    ).style.display = "none");
-    const clasificacion = (document.getElementById(
-      "id_clasificacion"
-    ).style.display = "none");
-    const resultados = (document.getElementById("id_resultados").style.display =
-      "block");
-
-    const btnResultados = document.getElementById("btnResultados");
-    btnResultados.classList.add("button-active");
-    const btnPartidos = document
-      .getElementById("btnPartidos")
-      .classList.remove("button-active");
-    const btnClasificacion = document
-      .getElementById("btnClasificacion")
-      .classList.remove("button-active");
-
-    getResults();
-  });
-
-const btnClasificacion = document
-  .getElementById("btnClasificacion")
-  .addEventListener("click", () => {
-    const partidos = (document.getElementById(
-      "id_proximos_partidos"
-    ).style.display = "none");
-    const clasificacion = (document.getElementById(
-      "id_clasificacion"
-    ).style.display = "block");
-    const resultados = (document.getElementById("id_resultados").style.display =
-      "none");
-
-    const btnClasificacion = document.getElementById("btnClasificacion");
-    btnClasificacion.classList.add("button-active");
-    const btnPartidos = document
-      .getElementById("btnPartidos")
-      .classList.remove("button-active");
-    const btnResultados = document
-      .getElementById("btnResultados")
-      .classList.remove("button-active");
-
-    getClassification();
-  });
+});
 
 async function getClassification() {
   try {
     const response = await fetch(classification);
     const data = await response.json();
     fillTable(data.results);
+    setTimeout(() => {
+      const buttons = [
+        "btnLastJornada",
+        "btnPenulJornada",
+        "btnAnteJornada",
+        "btnFirstJornada",
+      ];
+      buttons.forEach((btn, index) => {
+        const button = document.getElementById(btn);
+        index === 0
+          ? button.classList.add("active")
+          : button.classList.remove("active");
+      });
+    }, 525);
   } catch (error) {
     console.log(error);
   }
@@ -217,6 +194,12 @@ function displayMatches(jornada) {
     team1.id = "team-1";
     team1.textContent = partido.first_team.country;
 
+    // Create flag for first team
+    const flag1 = document.createElement("img");
+    flag1.src = `assets/img/${partido.first_team.country}.png`;
+    flag1.alt = partido.first_team.country + " flag";
+    flag1.classList.add("flag");
+
     const golesDiv = document.createElement("div");
     golesDiv.classList.add("goles");
 
@@ -233,6 +216,12 @@ function displayMatches(jornada) {
     team2.id = "team-2";
     team2.textContent = partido.second_team.country;
 
+    // Create flag for second team
+    const flag2 = document.createElement("img");
+    flag2.src = `assets/img/${partido.second_team.country}.png`;
+    flag2.alt = partido.second_team.country + " flag";
+    flag2.classList.add("flag");
+
     const fechaDiv = document.createElement("div");
     fechaDiv.classList.add("fecha");
 
@@ -243,7 +232,7 @@ function displayMatches(jornada) {
 
     // Agregar elementos al DOM
     golesDiv.append(golesTeam1, separator, golesTeam2);
-    encuentroDiv.append(team1, golesDiv, team2);
+    encuentroDiv.append(flag1, team1, golesDiv, team2, flag2);
     fechaDiv.appendChild(fechaP);
     partidoDiv.append(encuentroDiv, fechaDiv);
 
@@ -255,19 +244,127 @@ async function getPartidos() {
   try {
     const response = await fetch(matches);
     const data = await response.json();
-    console.log(data);
+
+    const prox_jornadas = Object.keys(data);
+
+    jornada_numbers_proximos.forEach((id, index) => {
+      const element = document.getElementById(id);
+      element.textContent = prox_jornadas[index];
+    });
+
+    btnsProxJornada.forEach((id, index) => {
+      const btn = document.getElementById(id);
+      btn.addEventListener("click", () => {
+        btnsProxJornada.forEach((otherId) => {
+          const otherBtn = document.getElementById(otherId);
+          otherBtn.classList.remove("active");
+        });
+        btn.classList.add("active");
+        const jornada = data[prox_jornadas[index]];
+        displayMatchesProx(jornada);
+      });
+    });
+
+    displayMatchesProx(data[prox_jornadas[0]]);
+
     setTimeout(() => {
-      const buttons = ["btnLastJornada", "btnPenulJornada", "btnAnteJornada", "btnFirstJornada"];
+      const buttons = [
+        "btnLastJornada",
+        "btnPenulJornada",
+        "btnAnteJornada",
+        "btnFirstJornada",
+      ];
       buttons.forEach((btn, index) => {
         const button = document.getElementById(btn);
-        index === 0 ? button.classList.add("active") : button.classList.remove("active");
+        index === 0
+          ? button.classList.add("active")
+          : button.classList.remove("active");
       });
-    }, 525);
-
+    }, 524);
   } catch (error) {
     console.log(error);
   }
 }
 
+function displayMatchesProx(jornada) {
+  const proximos_encuentros = document.getElementById("proximos_encuentros");
+
+  // Clear existing content
+  proximos_encuentros.innerHTML = "";
+
+  // Iterate over each match of the jornada
+  jornada.forEach((partido) => {
+    const encuentro = document.createElement("div");
+    encuentro.className = "proximo_encuentro";
+
+    const encuentroInfo = document.createElement("div");
+    encuentroInfo.className = "proximo_encuentro_info";
+
+    const team1Info = document.createElement("div");
+    team1Info.className = "team_info";
+
+    const team1Img = document.createElement("div");
+    team1Img.className = "team_info_img";
+
+    const team1Flag = document.createElement("img");
+    team1Flag.src = `assets/img/${partido.first_team}.png`;
+    team1Flag.alt = `Bandera de ${partido.first_team}`;
+
+    const teamContentName = document.createElement("div");
+    teamContentName.className = "team_content_name";
+
+    const team1Name = document.createElement("h3");
+    team1Name.id = "equipo_1";
+    team1Name.textContent = partido.first_team;
+
+    team1Img.appendChild(team1Flag);
+    team1Info.appendChild(team1Img);
+    teamContentName.appendChild(team1Name);
+    team1Info.appendChild(teamContentName);
+
+    const team2Info = document.createElement("div");
+    team2Info.className = "team_info";
+
+    const team2Img = document.createElement("div");
+    team2Img.className = "team_info_img";
+
+    const team2Flag = document.createElement("img");
+    team2Flag.src = `assets/img/${partido.second_team}.png`;
+    team2Flag.alt = `Bandera de ${partido.second_team}`;
+
+    const teamContentName2 = document.createElement("div");
+    teamContentName2.className = "team_content_name";
+
+    const team2Name = document.createElement("h3");
+    team2Name.id = "equipo_2";
+    team2Name.textContent = partido.second_team;
+
+    team2Img.appendChild(team2Flag);
+    team2Info.appendChild(team2Img);
+    teamContentName2.appendChild(team2Name);
+    team2Info.appendChild(teamContentName2);
+
+    encuentroInfo.appendChild(team1Info);
+    encuentroInfo.appendChild(team2Info);
+
+    const encuentroFecha = document.createElement("div");
+    encuentroFecha.className = "proximo_encuentro_fecha";
+
+    const fechaEncuentro = document.createElement("p");
+    fechaEncuentro.id = "fecha_encuentro_proximo";
+    fechaEncuentro.textContent = `Fecha: ${new Date(
+      partido.date
+    ).toLocaleDateString()}`;
+
+    encuentroFecha.appendChild(fechaEncuentro);
+
+    encuentro.appendChild(encuentroInfo);
+    encuentro.appendChild(encuentroFecha);
+
+    proximos_encuentros.appendChild(encuentro);
+  });
+}
+
 getClassification();
 getResults();
+getPartidos();
